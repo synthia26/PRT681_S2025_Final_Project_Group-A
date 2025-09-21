a3d9a7e18f5cd4d2f11d43c1c1251e16c23b05e8
tauhidatoul <70933993+tauhidatoul@users.noreply.github.com>
2025-09-21 19:45:19 +0930

    Delete Dev/Tauhid/

--- Dev/Tauhid/NTVibe.API/Dtos/OrganizerRegistrationDto.cs
+++
@@ -1,9 +0,0 @@
-namespace NTVibe.API.Dtos
-{
-    public class OrganizerRegistrationDto
-    {
-        public string OrganizationName { get; set; }
-        public string ContactEmail { get; set; }
-        public string Password { get; set; }
-    }
-}
